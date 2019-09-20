65d9b60d54c636920ec7c28a86f02bc6ef07e89f
sravs96123 <arutlasravya08@gmail.com>
2019-09-20 15:52:26 +0530

    This is for the new_branch created

--- hello.c
+++ hello.c
@@ -2,5 +2,6 @@
 
 int main(){
 printf("Welcome to the world of git for the second time and now for the third time");
+printf("I created a new branch to add this change to it and merge these two branch");
 return 0;
 }
