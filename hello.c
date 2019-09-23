af117047f36738cf086650285727c455ed885ac0
sravs96123 <arutlasravya08@gmail.com>
2019-09-23 11:37:38 +0530

    Removed all files

--- hello.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.c>
-
-int main(){
-printf("Welcome to the world of git for the second time and now for the third time");
-printf("I created a new branch to add this change to it and merge these two branch");
-return 0;
-}
